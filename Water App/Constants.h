bf640fce950c003ecd68d47b0a396f7e953107d3
ImmanuelKannan <immanueldk92@gmail.com>
2017-05-05 23:17:50 +0800

    Add placeholder colors for background gradient

--- Water App/Constants.h
+++ Water App/Constants.h
@@ -19,6 +19,10 @@
 
     #define kWhiteColor [UIColor whiteColor]
 
+    #define kColor2 [UIColor colorWithRed:20./255. green:48./255. blue:96./222. alpha:1.0]
+    #define kColor1 [UIColor colorWithRed:41./255. green:84./255. blue:153./222. alpha:1.0]
+    #define kFluidBackground [UIColor colorWithRed:27./255. green:55./255. blue:99./255. alpha:1.0]
+
 
 #pragma mark - Font & Sizes
 
